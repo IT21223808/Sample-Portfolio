1e950c32ae7d2bc5aedf1b0355fa2a071967e67b
IT21223808 <IT21223808@my.sliit.lk>
2024-09-15 12:53:07 +0530

    changes

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -3,7 +3,8 @@
 import React, { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial} from "@react-three/drei";
-// @ts-expect-error
+// @ts-expect-error: This is expected due to specific TypeScript version incompatibility
+
 import * as random from "maath/random/dist/maath-random.esm";
 
 const StarBackground = (props: any) => {
